ff90fa83089a045400eb4c5f82160a65d13f29da
kleima <kleima.serial@gmail.com>
2022-10-14 21:54:00 -0500

    create view for role control

---
+++ resources/js/Pages/Role/Create.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {useForm} from '@inertiajs/inertia-vue3';
+
+const form=useForm({
+    role:''
+});
+
+const submit=()=>{
+    form.post(route('roles.save'));
+}
+
+</script>
+
+<template>
+    <form @submit.prevent="submit">
+        <label for="">Nombre del rol</label>
+        <input type="text" v-model="form.role" />
+        <button type="submit">submit</button>
+    </form>
+</template>
---
+++ resources/js/Pages/Role/Index.vue
@@ -0,0 +1,10 @@
+<script setup>
+const {roles}=defineProps(['roles']);
+
+</script>
+<template>
+    <h1>Look at roles</h1>
+    <ul>
+        <li v-for="role in roles"><span>{{role.role}}</span><a :href="`/roles/delete/${role.id}`">delete</a></li>
+    </ul>
+</template>
