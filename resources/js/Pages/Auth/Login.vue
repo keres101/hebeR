e6684afbdfdd1dc4b947999546edab3964c66e66
keres <keres.kgb@gmail.com>
2022-11-09 10:35:57 -0500

    change gmail to user, auth

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -15,7 +15,7 @@ defineProps({
 });
 
 const form = useForm({
-    email: "",
+    name: "",
     password: "",
     remember: false,
 });
@@ -49,9 +49,9 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
-                <TextInput id="email" v-model="form.email" type="email" class="mt-1 block w-full" required autofocus />
-                <InputError class="mt-2" :message="form.errors.email" />
+                <InputLabel for="user" value="Usuario" />
+                <TextInput id="user" v-model="form.name" type="text" class="mt-1 block w-full" required autofocus />
+                <InputError class="mt-2" :message="form.errors.name" />
             </div>
 
             <div class="mt-4">
